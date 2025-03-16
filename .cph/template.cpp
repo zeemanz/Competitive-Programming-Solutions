e807fd1fe2634eb2a6beb71a1ffd67618d81f886
Zeeman Zhang <57309261+zeemanz@users.noreply.github.com>
2025-03-16 18:55:19 +0800

    设置C/C++和Competitive Programming Helper扩展

---
+++ .cph/template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using u32 = unsigned int;
+using i64 = long long;
+using u64 = unsigned long long;
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    return 0;
+}
